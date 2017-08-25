a7f8ac692eb43656be4393c02b8c91de10d232df
Xmaoyh <353804616@qq.com>
2017-08-25 17:24:55 +0800

    param

--- src/main/java/com/mao/girl/HelloController.java
+++ src/main/java/com/mao/girl/HelloController.java
@@ -2,13 +2,17 @@ package com.mao.girl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/hello")
 public class HelloController {
-//    @Value("${cupSize}")
+    //    @Value("${cupSize}")
 //    private String cupSize;
 //    @Value("${age}")
 //    private int age;
@@ -16,8 +20,20 @@ public class HelloController {
 //    private String content;
     @Autowired
     private GirlProperties mGirlProperties;
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String say() {
-        return mGirlProperties.getCupSize();
+
+//    @RequestMapping(value = "/say/{id}", method = RequestMethod.GET)
+//    public String say(@PathVariable("id") int id) {
+//        return mGirlProperties.getCupSize() + id;
+//    }
+
+//    @RequestMapping(value = "/say", method = RequestMethod.POST)
+//    public String say(@RequestParam("id") int id) {
+//        return mGirlProperties.getCupSize() + id;
+//    }
+
+    //@RequestMapping(value = "/say", method = RequestMethod.POST)
+    @GetMapping("/say")
+    public String say(@RequestParam(value = "id", required = false, defaultValue = "0") int id) {
+        return mGirlProperties.getCupSize() + id;
     }
 }
