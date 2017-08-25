aea5383f6303468a09b6d157d738398c9e015942
Xmaoyh <353804616@qq.com>
2017-08-25 16:25:29 +0800

    properties

---
+++ src/main/java/com/mao/girl/HelloController.java
@@ -0,0 +1,23 @@
+package com.mao.girl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+//    @Value("${cupSize}")
+//    private String cupSize;
+//    @Value("${age}")
+//    private int age;
+//    @Value("${content}")
+//    private String content;
+    @Autowired
+    private GirlProperties mGirlProperties;
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String say() {
+        return mGirlProperties.getCupSize();
+    }
+}
