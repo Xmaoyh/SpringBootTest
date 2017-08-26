dce2954c4c865dbed42a1b7a23dcdd7354c5e03d
Xmaoyh <353804616@qq.com>
2017-08-26 09:37:08 +0800

    db

--- src/main/java/com/mao/girl/HelloController.java
+++ src/main/java/com/mao/girl/HelloController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
